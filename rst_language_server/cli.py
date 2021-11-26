cb3e4184bdd9e7ac50c8e59b0af55c9270cd1482
Michael Seifert <m.seifert@digitalernachschub.de>
2021-11-26 09:15:54 +0100

    feat: Added the "--log-file" option to rst_ls.

--- rst_language_server/cli.py
+++ rst_language_server/cli.py
@@ -1,10 +1,20 @@
+import logging
+
 import click
 
 from rst_language_server.server import create_server
 
 
 @click.command("rst-ls")
-def rst_ls():
+@click.option(
+    "--log-file",
+    type=click.Path(dir_okay=False),
+    help="Writes log output to the specified file path",
+)
+def rst_ls(log_file):
+    file_handler = logging.FileHandler(filename=log_file)
+    pygls_logger = logging.getLogger("pygls")
+    pygls_logger.addHandler(file_handler)
     server_ = create_server()
     server_.start_io()
 
--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,3 +1,6 @@
+import logging
+from unittest.mock import patch
+
 from click.testing import CliRunner
 
 from rst_language_server.cli import rst_ls
@@ -10,3 +13,21 @@ def test_help_option_shows_help():
 
     assert result.exit_code == 0
     assert result.stdout
+
+
+def test_log_file_option_configures_file_for_pygls(tmp_path):
+    cli = CliRunner()
+    log_file = tmp_path / "output.log"
+
+    with patch("rst_language_server.server.create_server"):
+        cli.invoke(rst_ls, [f"--log-file={str(log_file)}"])
+
+    logger = logging.getLogger("pygls")
+    file_handlers = [
+        handler
+        for handler in logger.handlers
+        if isinstance(handler, logging.FileHandler)
+    ]
+    assert file_handlers
+    file_handler = file_handlers[0]
+    assert file_handler.baseFilename == str(log_file)
