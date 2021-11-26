906a9949e89438118040124a2f20828bddd8cbe9
Michael Seifert <m.seifert@digitalernachschub.de>
2021-11-26 09:26:48 +0100

    feat: Added the "--log-level" option to rst_ls.

--- rst_language_server/cli.py
+++ rst_language_server/cli.py
@@ -4,6 +4,11 @@ import click
 
 from rst_language_server.server import create_server
 
+_log_level_names = [
+    logging.getLevelName(level).lower()
+    for level in (logging.DEBUG, logging.INFO, logging.WARNING, logging.ERROR)
+]
+
 
 @click.command("rst-ls")
 @click.option(
@@ -11,9 +16,16 @@ from rst_language_server.server import create_server
     type=click.Path(dir_okay=False),
     help="Writes log output to the specified file path",
 )
-def rst_ls(log_file):
+@click.option(
+    "--log-level",
+    type=click.Choice(_log_level_names),
+    default="info",
+    help="Sets the minimum severity for log output",
+)
+def rst_ls(log_file, log_level: str):
     file_handler = logging.FileHandler(filename=log_file)
     pygls_logger = logging.getLogger("pygls")
+    pygls_logger.setLevel(log_level.upper())
     pygls_logger.addHandler(file_handler)
     server_ = create_server()
     server_.start_io()
--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,6 +1,7 @@
 import logging
 from unittest.mock import patch
 
+import pytest
 from click.testing import CliRunner
 
 from rst_language_server.cli import rst_ls
@@ -15,7 +16,7 @@ def test_help_option_shows_help():
     assert result.stdout
 
 
-def test_log_file_option_configures_file_for_pygls(tmp_path):
+def test_log_file_option_configures_file_for_pygls_logger(tmp_path):
     cli = CliRunner()
     log_file = tmp_path / "output.log"
 
@@ -31,3 +32,25 @@ def test_log_file_option_configures_file_for_pygls(tmp_path):
     assert file_handlers
     file_handler = file_handlers[0]
     assert file_handler.baseFilename == str(log_file)
+
+
+@pytest.mark.parametrize(
+    "log_level",
+    [
+        logging.getLevelName(level).lower()
+        for level in (logging.DEBUG, logging.INFO, logging.WARNING, logging.ERROR)
+    ],
+)
+def test_log_level_option_configures_level_for_pygls_logger(tmp_path, log_level: str):
+    cli = CliRunner()
+    log_file = tmp_path / "output.log"
+
+    with patch("rst_language_server.server.create_server"):
+        cli.invoke(
+            rst_ls,
+            [f"--log-file={str(log_file)}", f"--log-level={log_level}"],
+            catch_exceptions=False,
+        )
+
+    logger = logging.getLogger("pygls")
+    assert logging.getLevelName(logger.level) == log_level.upper()
